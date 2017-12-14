0275e4a8ee6353377960e55e1181352d7e093048
DONGNAN-ZHANG <s3682306@student.rmit.edu.au>
2017-12-14 16:10:19 +1100

    added marquee

--- a2/index.php
+++ a2/index.php
@@ -3,7 +3,14 @@
 <body>
     <header></header>
     <nav></nav>
-    <main>webpage under construction</main>
+    
     <footer></footer>
+    <marquee style="color:firebrick;" direction="down" width="100%" height="500" behavior="alternate" style="border:solid">
+        <marquee behavior="alternate">
+            <div><marquee style="color: blue;" direction="up">&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;</marquee></div>
+            <main>webpage under construction</main>
+            <div><marquee style="color: blue;" direction="up">&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;</marquee></div>
+        </marquee>
+    </marquee>
 </body>
 </html>
