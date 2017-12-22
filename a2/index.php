e9c9699c984811cd684066635a17219cf4f7e3e8
DONGNAN-ZHANG <s3682306@student.rmit.edu.au>
2017-12-22 21:57:22 +1100

    done a1

--- a2/index.php
+++ a2/index.php
@@ -3,14 +3,15 @@
 <body>
     <header></header>
     <nav></nav>
-    
-    <footer></footer>
-    <marquee style="color:firebrick;" direction="down" width="100%" height="500" behavior="alternate" style="border:solid">
-        <marquee behavior="alternate">
-            <div><marquee style="color: blue;" direction="up">&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;</marquee></div>
-            <main>webpage under construction</main>
-            <div><marquee style="color: blue;" direction="up">&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;</marquee></div>
+    <main>
+        <marquee style="color:firebrick;" direction="down" width="100%" height="500" behavior="alternate" style="border:solid">
+            <marquee behavior="alternate">
+                <div><marquee style="color: blue;" direction="up">&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;</marquee></div>
+                <main>webpage under construction</main>
+                <div><marquee style="color: blue;" direction="up">&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;</marquee></div>
+            </marquee>
         </marquee>
-    </marquee>
+    </main>
+    <footer></footer>
 </body>
 </html>
