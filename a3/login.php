5a4b1d5c5b67a04ef06379a1ca9b781688c4f97f
DONGNAN-ZHANG <s3682306@student.rmit.edu.au>
2018-02-15 23:43:41 +1100

    a3

--- a3/login.php
+++ a3/login.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
- 
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,7 +15,9 @@
 <body>
     <header class="header">
         <div class="header-text">
-            <h1>Pizza workshop</h1>
+            <h1>
+                <a>Pizza workshop</a>
+            </h1>
         </div>
         <div class="header-color">
             <div class="header-color-green"></div>
@@ -48,30 +50,18 @@
         </div>
     </nav>
     <main>
-        <div class="section product">
+        <section class="section login">
             <div class="container">
-                <img src="../img/veggie.png" alt="veggie" class="product-img" />
-                <aside>
-                    <h1>Veggie</h1>
-                    <p>Our Veggie recipe is perfect for those who love all the colors of the vegetable spectrum – tomato sauce,
-                        100% real cheese, red onions, green peppers, mushrooms and black olives.</p>
-                    <h2>$14.99</h2>
-                    <form class="product-form" action="https://titan.csit.rmit.edu.au/~e54061/wp/processing.php?ref=product" method="post">
-                        <label for="option">Way of Delivery</label>
-                        <select name="option" id="method">
-                            <option value="takeaway">Take Away</option>
-                            <option value="delivery">Delivery</option>
-                        </select>
-                        <label for="qty">Number of Pizzas</label>
-                        <input style="display: none" name="id" value="1" />
-                        <button id="minus">-</button>
-                        <input value=1 name="qty" id="quantity" />
-                        <button id="plus">+</button>
-                        <input type="submit" value="add" />
-                    </form>
-                </aside>
+                <form action="https://titan.csit.rmit.edu.au/~e54061/wp/processing.php?ref=login" method="post" class="login-form">
+                    <h1>Login</h1>
+                    <label for="">Email:</label>
+                    <input type="email" name="email" placeholder="Please enter your email..." />
+                    <label for="">Password:</label>
+                    <input type="password" name="password" placeholder="Please enter your password..." />
+                    <input type="submit" />
+                </form>
             </div>
-        </div>
+        </section>
     </main>
     <footer class="footer">
         <div class="container">
@@ -82,7 +72,7 @@
             <p>ID: 3682306</p>
         </div>
     </footer>
-    <script src="js/product.js"></script>
+    <script src="js/login.js"></script>
 </body>
 
 </html>
